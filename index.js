101e0f10c1477f41d1adc8680e3d6495c42e6552
Oskar Yildiz <yildizoskar@gmail.com>
2018-02-08 20:59:51 +0100

    Cleaning

--- index.js
+++ index.js
@@ -2,54 +2,39 @@ const cheerio = require('cheerio');
 const superagent = require('superagent');
 
 const getHtml = async url => {
-  let html = await superagent.get(url);
+  const html = await superagent.get(url);
   return html.res.text;
 };
 
-const formatFriday = string => {
-  let fixedString = string.split(/(Kött|Fisk|Veggi)\b/g);
-  console.log(fixedString, 'FIXED');
-  return fixedString;
-};
-
-const findMeny = async () => {
+/**
+ * Gets the weekly lunch menu from kista.eatery.se
+ */
+export const getMenu = async () => {
   const data = await getHtml('http://kista.eatery.se');
-  let $ = cheerio.load(data);
-  let fullSidebar = $('.sidebar').text();
-  let sideBar = fullSidebar.split(/(Måndag|Tisdag|Onsdag|Torsdag|Fredag)\b/g);
+  const $ = cheerio.load(data);
+  const fullSidebar = $('.sidebar').text();
+  const sideBar = fullSidebar.split(/(Måndag|Tisdag|Onsdag|Torsdag|Fredag)\b/g);
   const weekDays = ['Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag'];
-  let fullJson = {};
-  let formattedDays = [];
+  const fullJson = {};
+  const formattedDays = [];
   for (let i = 0; i < sideBar.length; i++) {
     fullJson[weekDays[i]] = {};
     if (i % 2 === 0 && i !== 0) {
-      let weekday = i - 2;
+      // let weekday = i - 2;
       let formatedDay = sideBar[i].split('\n', 6);
       formatedDay = formatedDay.filter(e => e !== '');
       formatedDay.shift();
       formatedDay.pop();
 
-      for (var j = 0; j < formatedDay.length; j++) {
+      for (let j = 0; j < formatedDay.length; j++) {
         formatedDay[j].split(': ', 3);
-        // fullJson[weekday] = {
-        //   Fisk: formatedDay[j].split(' ', 1)
-        //
-        // };
       }
       formattedDays.push(formatedDay);
-      console.log(formatedDay);
     }
   }
-  console.log(JSON.stringify(fullJson, null, 2));
-  console.log('fulljson', fullJson);
   const menu = {};
-  for (let day in formattedDays) {
-    menu[weekDays[day]] = formattedDays[day];
+  for (let i = 0; i < formattedDays.length; i++) {
+    menu[weekDays[i]] = formattedDays[i];
   }
-  let lastLine = sideBar[8].split(/(Kött|Fisk|Veggi)\b/g);
-  // console.log(lastLine[2].split(':', 2));
-  // console.log(lastLine[6].split('\n', 1));
   return menu;
 };
-
-console.log(findMeny());
